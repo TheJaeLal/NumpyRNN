c69f629b38d0d75f044a62da7f3ac9a7ad4b3d1e
thejaelal <jay.lal@somaiya.edu>
2018-01-30 21:28:08 +0530

    Cleaned up the data file for infrequent,characters and mismatches

--- RNN.py
+++ RNN.py
@@ -1,11 +1,19 @@
 #Breakdown text into Characters
-#filename = "Immortals_of_Meluha.txt"
-filename = "test.txt"
+filename = "Immortals_of_Meluha.txt"
+#filename = "test.txt"
 
 with open(filename,'r') as f:
 	content = f.read()
 
 lines = list(filter(None,content.split('\n')))
 
+
 # print(lines)
 
+char_set = set()
+
+for line in lines:
+	for char in line:
+		char_set.add(char)
+
+print(char_set)
