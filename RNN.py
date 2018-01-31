928ad391564527cd70b57010797ccb2659bf5b8e
thejaelal <jay.lal@somaiya.edu>
2018-01-31 20:59:54 +0530

    Removed encode(), made a few optimizations

--- RNN.py
+++ RNN.py
@@ -4,6 +4,8 @@ class Network:
 	def __init__(self,nh,nc):
 		#Dimensions of Wxh,Whh,Why = [(nh,nc),(nh,nh),(nc,nh)]
 
+		self.nc = nc
+		self.nh = nh
 		self.Wxh = np.random.randn(nh,nc) * np.sqrt(1/nc)
 		self.Whh = np.random.randn(nh,nh) * np.sqrt(1/nh)
 		self.Why = np.random.randn(nc,nh) * np.sqrt(1/nh)
@@ -14,20 +16,32 @@ class Network:
 	def forward_pass(self,X,vocabulary):
 
 		#Sequence is a list of one-hot vectors, each representing a character.
-		sequence = encode(X,vocabulary)
-		
+		#sequence = encode(X,vocabulary)
+		#print(sequence)
+
 		output = []	
-		for xt in sequence:
+
+		#len(X)+1 because we need a <start> token
+		sequence = [0]*(len(X)+1)
+
+		for i,xt in enumerate(X):
+			sequence[i+1] = vocabulary.index(xt)
+
+		#print("Sequence = ",sequence)
+
+		for t in sequence:
 			
 			#Update the hidden state
-			self.state = np.dot(self.Wxh,xt) + np.dot(self.Whh,self.state)
+			self.state = np.tanh(self.Wxh[:,t].reshape(self.nh,1) + np.dot(self.Whh,self.state))
 			
 			#Calculate Output
 			yt = np.dot(self.Why,self.state)
 			
+			#print("shape of (state,yt) =({0},{1})".format(self.state.shape,yt.shape))			
 			#Squash to provide a probability distribution between 0 and 1
 			ot = softmax(yt)
 
+			#print("ot =",ot)
 			#prediction = np.argmax(ot,axis=1)
 			output.append(ot)
 
@@ -38,31 +52,9 @@ def softmax(yt):
 	return exp_yt/float(np.sum(exp_yt))
 
 def predict(ot):
-	return np.argmax(ot,axis=0)
-
-def encode(X,vocabulary):
-
-	nc = len(vocabulary)
-
-	sequence = []
-	
-	#Append the <start> token to indicate start of the sequence/paragraph
-	one_hot_start = np.zeros((nc,1))
-	one_hot_start[0] = 1
-	sequence.append(one_hot_start)
-
-	for xt in X:
-		char_idx = vocabulary.index(xt)
-		one_hot_xt = np.zeros((nc,1))
-		one_hot_xt[char_idx] = 1
-		sequence.append(one_hot_xt)
-	
-	#Append the <end> token to indicate end of the sequence/paragaraph
-	one_hot_end = np.zeros((nc,1))
-	one_hot_end[-1] = 1
-	sequence.append(one_hot_end)
-
-	return sequence
+	#print("ot =",ot)
+	#print("Finding argmax of",ot)
+	return np.argmax(ot,axis=0)[0]
 
 def load_data(filename):
 
@@ -71,7 +63,7 @@ def load_data(filename):
 
 	lines = list(filter(None,content.split('\n')))
 
-	print(lines)
+	#print(lines)
 
 	char_set = set()
 
@@ -79,32 +71,36 @@ def load_data(filename):
 		for char in line:
 			char_set.add(char)
 
-	for i,char in enumerate(char_set):
-		print("{0}:{1}".format(i,char))
+	#for i,char in enumerate(char_set):
+		#print("{0}:{1}".format(i,char))
 
 	return lines,list(char_set)
 
 
 def main():
-	#filename = "Immortals_of_Meluha.txt"
-	filename = "hello.txt"
+	filename = "Immortals_of_Meluha.txt"
+	#filename = "hello.txt"
 
 	data,vocabulary = load_data(filename)
 
 	vocabulary = ['<start>'] + vocabulary + ['<end>']
 
-	# for no,line in enumerate(data):
-	# 	print("{0}:{1}".format(no,line))
+	#print("vocabulary =",vocabulary)
+	#for no,line in enumerate(data):
+		#print("{0}:{1}".format(no,line))
 
 	#data is a list of lines, each line is a string, i.e a list of characters
 	my_rnn = Network(nh=100,nc=len(vocabulary))
 
 	line = data[0]
-	output = my_rnn.forward_pass(line,vocabulary)
-	print(output)
-	print("Predictions")
-	for char in output:
-		print(vocabulary[int(predict(char))])
+	for line in data:
+		output = my_rnn.forward_pass(line,vocabulary)
+		# print("Predictions Start here")
+		# for char in output:
+		# 	prediction = predict(char)
+		# 	#print("Prediction =",pred)
+		# 	#print(vocabulary[prediction],end="")
+		# print("")
 
 if __name__ == '__main__':
 	main()
